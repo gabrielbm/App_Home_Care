a4e2cffe93d0b3c9e2f283f53c6a1d86e7f61d3b
Vinicius Frasson <vfrasson1303@hotmail.com>
2018-03-21 19:54:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/frasson/app_homecare/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.frasson.app_homecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
