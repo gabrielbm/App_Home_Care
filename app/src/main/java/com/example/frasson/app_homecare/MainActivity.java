7f9d274c87347770855d1672214f0f4d1dcdffd2
Vinicius Frasson <vfrasson1303@hotmail.com>
2018-03-21 21:13:30 -0300

    Commit no objeto do "buttonCadastroCliente" para chamar uma pagina de cadastro!

--- app/src/main/java/com/example/frasson/app_homecare/MainActivity.java
+++ app/src/main/java/com/example/frasson/app_homecare/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button cadastroCliente = findViewById(R.id.buttonCadastroCliente);
+
 
 
     }
